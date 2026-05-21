3aa565437a9a7e7626edd2ac967f099a6a7ec3a5
LiangChen-Ricky <rickyrockup@gmail.com>
2026-05-21 22:43:36 +1200

    项目1: 添加对赌完成率统计

--- analyzer.py
+++ analyzer.py
@@ -6,5 +6,11 @@ df = pd.read_csv('students.csv')
 # 先看看数据长什么样
 print(df)
 print("\n总行数:", len(df))
+total = len(df)
 print(df['country'].value_counts())
 df['country'].value_counts().to_dict()
+counts = df['bet_status'].value_counts().to_dict()
+completed = counts['completed']
+completion_rate = round(completed / total * 100, 1)
+
+print(f"对赌完成率: {completion_rate}%")
