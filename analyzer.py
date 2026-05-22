11278daa9f4837215603cc68c7f7b3cf4f5b14fe
LiangChen-Ricky <rickyrockup@gmail.com>
2026-05-22 19:29:32 +1200

    项目1完成: CSV分析器+输出report.json

--- analyzer.py
+++ analyzer.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import json
 
 # 读取CSV
 df = pd.read_csv('students.csv')
@@ -9,8 +10,19 @@ print("\n总行数:", len(df))
 total = len(df)
 print(df['country'].value_counts())
 df['country'].value_counts().to_dict()
+country_count = df['country'].value_counts().to_dict()
 counts = df['bet_status'].value_counts().to_dict()
 completed = counts['completed']
 completion_rate = round(completed / total * 100, 1)
 
 print(f"对赌完成率: {completion_rate}%")
+report = {
+    'total': total,
+    'country_count': country_count,
+    'completion_rate': completion_rate
+}
+
+with open('report.json', 'w') as f:
+    json.dump(report, f)
+
+print("报告已保存到 report.json")
